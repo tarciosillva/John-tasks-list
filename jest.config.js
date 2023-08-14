adea31183f43ce2b733fc09dbb316e032b26b576
tarciosillva <tsilva@bluelab.com.br>
2023-08-14 17:46:52 -0300

    Defined jest configs

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+const nextJest = require('next/jest')
+const createJestConfig = nextJest({ dir: './' })
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+}
+module.exports = createJestConfig(customJestConfig)
